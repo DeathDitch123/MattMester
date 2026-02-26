185e97bb124667066b2704dd9c32a5ad41137d2a
Barold01 <zsoltsertes03@gmail.com>
2026-02-26 04:30:15 +0100

    Bábu képútvonalak objektum dokumentálása
    A PIECE_IMAGES objektum literál átnevezése babuKepek-re és soronkénti kommentekkel ellátása, amely a 12 sakkfigura (6 fehér, 6 fekete) képfájljainak elérési útját tárolja szín és típus szerinti struktúrában.

---
+++ frontend/chess_barold/javascript/chess.js
@@ -0,0 +1,25 @@
+// Objektum literál: Bábu képek tárolása
+// Létrehoz egy objektumot ami tárolja az összes bábu kép elérési útját (12 kép: 6 fehér + 6 fekete)
+
+
+let babuKepek = {
+    white:
+    {
+        king: "images/white_king.png",      // Fehér király képfájljának elérési útja
+        queen: "images/white_queen.png",    // Fehér vezér képfájljának elérési útja
+        rook: "images/white_rook.png",      // Fehér bástya képfájljának elérési útja
+        bishop: "images/white_bishop.png",  // Fehér futó képfájljának elérési útja
+        knight: "images/white_knight.png",  // Fehér huszár képfájljának elérési útja
+        pawn: "images/white_pawn.png"       // Fehér gyalog képfájljának elérési útja
+    },
+
+    black:
+    {
+        king: "images/black_king.png",      // Fekete király képfájljának elérési útja
+        queen: "images/black_queen.png",    // Fekete vezér képfájljának elérési útja
+        rook: "images/black_rook.png",      // Fekete bástya képfájljának elérési útja
+        bishop: "images/black_bishop.png",  // Fekete futó képfájljának elérési útja
+        knight: "images/black_knight.png",  // Fekete huszár képfájljának elérési útja
+        pawn: "images/black_pawn.png"       // Fekete gyalog képfájljának elérési útja
+    }
+};
