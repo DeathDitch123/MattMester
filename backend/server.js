2cd7e5a1992056246c9d8c9e855e6acb2132a148
Sigaba <berta.botond@trefortszki.hu>
2026-03-08 21:27:30 +0100

    server.js struktúra rendezés

--- backend/server.js
+++ backend/server.js
@@ -12,25 +12,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const router = express.Router();
-
 const ip = '127.0.0.1';
 const port = 3000;
-
-//?Szívverés indítása a statisztikák frissítéséhez
-services.startHeartbeat(io); //?Heartbeat indítása a statisztikák frissítéséhez
-io.on('connection', (socket) => {
-    console.log('Új Socket.io kapcsolat létrejött:', socket.id);
-    services.handleConnection(socket, io);
-
-    socket.on('heartbeat', (socket) => {
-        // services.refreshStats(io); //?Statisztikák frissítése minden kliensnek a heartbeat eseményre
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Socket.io kapcsolat megszakadt:', socket.id);
-    });
-});
 
 //?Session beállítása
 const sessionSecret = process.env.SESSION_SECRET || 'chu+)2_23iIa6sou&>#o79247r9Xbsibv%';
@@ -47,34 +30,34 @@ const sessionMiddleware = session({
 //!Session beállítása:
 app.use(sessionMiddleware);
 io.engine.use(sessionMiddleware); //?Socket.io session kezelés
-
 app.use(express.json()); //?Middleware JSON
 app.set('trust proxy', 1); //?Middleware Proxy
-
-//!Routing
-//?Főoldal:
-router.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '../frontend/html/index.html'));
-});
-
-router.get('/register', (request, response) => {
-    response.sendFile(path.join(__dirname, '../frontend/html/register.html'));
-});
-
-router.get('/login', (request, response) => {
-    response.sendFile(path.join(__dirname, '../frontend/html/login.html'));
-});
-//!API endpoints
-app.use('/', router);
-const endpoints = require('./api/api.js');
-app.use('/api', endpoints);
-
 
 //!Szerver futtatása
 app.use(express.static(path.join(__dirname, '../frontend'))); //?frontend mappa tartalmának betöltése az oldal működéséhez
 
-//?Socket.io heartbeat kezelése
-const services = require('./services.js');
+//!Routing
+//?Főoldal:
+const endpoints = require('./api/api.js');
+app.use('/api', endpoints);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/html/index.html'));
+});
+
+//Socket.io események
+services.startHeartbeat(io); //?Heartbeat indítása a statisztikák frissítéséhez
+io.on('connection', (socket) => {
+    console.log('Új Socket.io kapcsolat létrejött:', socket.id);
+    services.handleConnection(socket, io);
+
+    socket.on('heartbeat', (socket) => {
+        // services.refreshStats(io); //?Statisztikák frissítése minden kliensnek a heartbeat eseményre
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Socket.io kapcsolat megszakadt:', socket.id);
+    });
+});
 
 // Adatbázis inicializálása, majd szerver indítása
 initDatabase()
